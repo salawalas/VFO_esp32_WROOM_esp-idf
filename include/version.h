8745a924499e746ba4930008a86e544925d3561e
salawalas <salawalas@gmail.com>
2026-04-02 22:48:54 +0200

    Zmiana werski commita.

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.1"
-#define FW_COMMIT  "cc0ddaa"
-#define BUILD_DATE "2026-04-02 22:32"
+#define FW_COMMIT  "909dcc4"
+#define BUILD_DATE "2026-04-02 22:36"
