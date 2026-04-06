8f3937464f9072a8e44d28270871a372bec2882f
salawalas <salawalas@gmail.com>
2026-04-06 21:34:08 +0200

    Usunięcie workflow/build.yml.

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.1"
-#define FW_COMMIT  "24d0a2f"
-#define BUILD_DATE "2026-04-06 19:26"
+#define FW_COMMIT  "8a16083"
+#define BUILD_DATE "2026-04-06 21:31"
