8642e409473f85b9f6ad128c4760e8d3be902f39
salawalas <salawalas@gmail.com>
2026-04-06 12:10:55 +0200

    Zmiany, dodanie środowisk : release i debug

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.1"
-#define FW_COMMIT  "f84f99a"
-#define BUILD_DATE "2026-04-05 09:58"
+#define FW_COMMIT  "e1e24a0"
+#define BUILD_DATE "2026-04-06 11:56"
