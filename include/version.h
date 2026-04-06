8a16083e4ac915eced6e39559b5a0ed9d4409a96
salawalas <salawalas@gmail.com>
2026-04-06 19:28:23 +0200

    Last  fix.

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.1"
-#define FW_COMMIT  "e1e24a0"
-#define BUILD_DATE "2026-04-06 11:56"
+#define FW_COMMIT  "24d0a2f"
+#define BUILD_DATE "2026-04-06 19:26"
