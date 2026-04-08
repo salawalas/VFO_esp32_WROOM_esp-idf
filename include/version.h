0708e4198b6a2b651af5a71f5063131c0737a9ec
salawalas <salawalas@gmail.com>
2026-04-08 21:55:01 +0200

    Zmiana , dodanie settings.json.

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.1"
-#define FW_COMMIT  "8a16083"
-#define BUILD_DATE "2026-04-06 21:31"
+#define FW_COMMIT  "abe8547"
+#define BUILD_DATE "2026-04-08 21:53"
