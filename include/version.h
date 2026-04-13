1c5b2343ad45ede9f0bac743ac2e11146efbbda4
salawalas <salawalas@gmail.com>
2026-04-13 23:01:31 +0200

    Zmiana w Cmakelist.txt pod esp-idf.

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.1"
-#define FW_COMMIT  "abe8547"
-#define BUILD_DATE "2026-04-08 21:53"
+#define FW_COMMIT  "0708e41"
+#define BUILD_DATE "2026-04-08 22:38"
