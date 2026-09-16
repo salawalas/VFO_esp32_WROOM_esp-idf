dba1c13898a55fd88eedb0479dbc793cf64a624a
salawalas <salawalas@gmail.com>
2026-09-16 22:45:00 +0200

    Poprawki w plikach schematu i pcb Kicada.

--- include/version.h
+++ include/version.h
@@ -4,5 +4,5 @@
 #define FW_NAME    "ESP32 VFO"
 #define FW_AUTHOR  "Marcin"
 #define FW_VERSION "2.2.0"
-#define FW_COMMIT  "2417eb0"
-#define BUILD_DATE "2026-09-16 18:23"
+#define FW_COMMIT  "b49a655"
+#define BUILD_DATE "2026-09-16 18:59"
